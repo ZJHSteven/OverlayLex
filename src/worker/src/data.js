6e6296b704121067018360057be45efc3fa4a76f
Steven ZJH <zhangjiahe0830@gmail.com>
2026-02-15 23:06:29 +0800

    fix(release): 对齐theatre与dummysheet版本

--- src/worker/src/data.js
+++ src/worker/src/data.js
@@ -127,7 +127,7 @@ export const PACKAGE_CATALOG = {
     "id": "obr-theatre-battle-system-com",
     "name": "OBR 自动翻译包 - theatre.battle-system.com",
     "kind": "translation",
-    "version": "0.1.2",
+    "version": "0.1.4",
     "enabledByDefault": true,
     "description": "OBR 自动翻译包 - theatre.battle-system.com（自动同步）"
   },
@@ -135,7 +135,7 @@ export const PACKAGE_CATALOG = {
     "id": "obr-www-dummysheet-com",
     "name": "OBR 自动翻译包 - www.dummysheet.com",
     "kind": "translation",
-    "version": "0.1.2",
+    "version": "0.1.4",
     "enabledByDefault": true,
     "description": "OBR 自动翻译包 - www.dummysheet.com（自动同步）"
   },
